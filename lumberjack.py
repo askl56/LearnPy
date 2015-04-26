2c1f749bb8d3f8d13c84ec0b0b8ef90c12b733cb
askl56 <amscott@openmailbox.org>
2015-04-26 12:42:10 +0100

    started learning python

---
+++ lumberjack.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+
+if name == "Andrew":
+    print("{} is a lumberjack and he/she is ok!".format(name))
+else:
+    print("{} sleeps all night and {} works all day!".format(name, name))
+
